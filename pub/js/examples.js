dcc56b76c8d3d0daaceb7d27433d907e915050e7
Henry Wang <henryhe.wang@mail.utoronto.ca>
2021-03-21 23:06:16 -0400

    added basic fishtank rendering

---
+++ pub/js/examples.js
@@ -0,0 +1,9 @@
+/* JS Library usage examples */
+"use strict";
+
+const ft = new Tank();
+
+ft.test();
+ft.RenderTank()
+ft.AddFish('assets/fish.svg')
+ft.AddFish('assets/fish.svg')
--- pub/js/fishtank.js
+++ pub/js/fishtank.js
@@ -1,21 +1,41 @@
 'use strict';
 const log = console.log;
 
-(function (global) {
 
-        const fishImage = 'assets/fish.svg'
+const fishImage = 'assets/fish.svg'
 
-        class Tank {
-            constructor() {
+class Tank {
+    constructor() {
+        this.tankArea = null
+        this.renderStatus = false
 
-            }
-        }
-
-        class Fish {
-            contructor() {
-
-            }
-        }
+        // Array containing all the fish in the tank
+        this.fishList = []
     }
-)
-(window);
+
+    RenderTank() {
+        this.renderStatus = true
+        const tankDiv = document.createElement('div')
+        tankDiv.style = 'border-style: solid; border-color: black'
+
+        this.tankArea = tankDiv
+
+        const body = document.querySelector('body')
+        body.append(tankDiv)
+    }
+
+    // Adds a fish to the tank from the img path "fish_path"
+    AddFish(fish_path) {
+        // Check that the tank is rendered
+        if (this.renderStatus === false) {
+            window.alert("Tank not rendered!")
+        }
+        const fish_pic = document.createElement('img')
+        fish_pic.src = fish_path
+
+        this.tankArea.append(fish_pic)
+        this.fishList.push(fish_pic)
+        log("Added fish to tank! " + this.fishList.length + " fish currently in tank.")
+    }
+}
+
