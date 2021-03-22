60f5e410616d34c904d52e058a34b22b3b9cf325
Henry Wang <henryhe.wang@mail.utoronto.ca>
2021-03-21 22:39:18 -0400

    setup project folders

---
+++ pub/js/fishtank.js
@@ -0,0 +1,21 @@
+'use strict';
+const log = console.log;
+
+(function (global) {
+
+        const fishImage = 'assets/fish.svg'
+
+        class Tank {
+            constructor() {
+
+            }
+        }
+
+        class Fish {
+            contructor() {
+
+            }
+        }
+    }
+)
+(window);
