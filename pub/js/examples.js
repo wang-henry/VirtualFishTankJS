434c0ac5a5bbf15a094aa07d0acdbcb3cdd8339b
Henry Wang <henryhe.wang@mail.utoronto.ca>
2021-04-17 20:09:07 -0400

    finished example code

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -32,24 +32,36 @@ t2.changeWater('green')
 t2.changeBorder('blue')
 
 //------------------------------------------------------------------------------------------------
-// Third fishtank
+// Initialize tank and render
 const t3 = new Tank(1000, 300, document.getElementById('tank3'));
 t3.renderTank()
+
+// Add fish and update orientation
 t3.addFish('assets/fish.svg', 0, 20, 40, 0, 20)
 t3.addFish('assets/fish.svg', 0, 40, 20, 10, 20)
 t3.addFish('assets/fish.svg', 0, 40, 20, 10, 50)
 t3.addFish('assets/fish.svg', 0, 60, 5, 5, 20)
 t3.updateAllHOrientation()
+
+// Enable movement
 t3.enableAllMove()
 
+
+// Create new tank and render
 const m1 = new Tank(1000, 300, document.getElementById('m1'));
 m1.renderTank()
+
+// Add fish
 m1.addFish('assets/fish.svg', 0, 0, 20, 0, 50, false)
 m1.addFish('assets/fish.svg', 0, 0, 20, 0, 50, false)
 
+// Set first fish to normal loop
 m1.setTravelPoints(0, 0, 500, 50, true, 'normal')
+
+// Set second fish to bounce loop
 m1.setTravelPoints(1, 0, 500, 100, true, 'bounce')
 
+// Flip all horizontal orientation and enable movement
 m1.updateAllHOrientation()
 m1.enableAllMove()
 
@@ -79,13 +91,17 @@ t4.updateAllHOrientation(0)
 t4.addText("<h1>Loading...<h1>", 0, 110)
 t4.enableAllMove()
 
+// Create new tank and render
 const t5 = new Tank(1000, 300, document.getElementById('tank5'));
 t5.renderTank()
+
+// Add some stationary objects
 t5.addStationaryObj('assets/rock.svg', 0, 0, '70px', '70px')
 t5.addStationaryObj('assets/rock.svg', 900, 0, '100px', '100px')
 t5.addStationaryObj('assets/rock.svg', 500, 0, '50px', '100px')
 t5.addStationaryObj('assets/seaweed.svg', 500, 0, '100px', '100px')
 t5.addStationaryObj('assets/seaweed.svg', 100, 0, '100px', '200px')
+
+// Add a fish and allow it to be user controlled
 t5.addFish('assets/fish.svg', 0, 0, 20, 20, 50, true)
 t5.updateAllHOrientation()
-t5.enableAllMove()
